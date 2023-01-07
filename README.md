1658688474382f8c7eddb8dd44b887693e9411c6
SHctt <SHcttMBG@protonmail.com>
2023-01-07 23:28:16 +0800

    设计表单元素样式（form.css, button.css）

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## 3.1 定义登录路由
 
 ## 3.2 定义登录组件（AuthLogin）
+
+## 3.3 设计表单元素样式（form.css, button.css）
--- src/auth/login/auth-login.vue
+++ src/auth/login/auth-login.vue
@@ -3,8 +3,20 @@
     <div class="form">
       <h1 class="header">用户登陆</h1>
       <div class="field">
-        <input type="text" placeholder="用户名" v-model="name" />
-        <input type="password" placeholder="密码" v-model="password" />
+        <input
+          class="input text"
+          type="text"
+          placeholder="用户名"
+          v-model="name"
+        />
+      </div>
+      <div class="field">
+        <input
+          class="input text"
+          type="password"
+          placeholder="密码"
+          v-model="password"
+        />
       </div>
       <div class="field">
         <button class="button large" @click="onClickLoginButton">登陆</button>
