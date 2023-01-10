397f838e0a546b2435561b4293801d1432eebf56
SHctt <SHcttMBG@protonmail.com>
2023-01-10 17:21:07 +0800

    创建 user-show 组件，并定义路由

--- src/app/app.store.ts
+++ src/app/app.store.ts
@@ -1,20 +1,19 @@
 import { createStore } from 'vuex';
-import { postStoreModule, PostStoreState } from '../post/post.store';
+import { postStoreModule, PostStoreState } from '@/post/post.store';
+import { userStoreModule, UserStoreState } from '@/user/user.store';
+import { authStoreModule, AuthStoreState } from '@/auth/auth.store';
 import { layoutStoreModule, LayoutStoreState } from './layout/layout.store';
-import { localStorageStorePlugin } from './app.store.plugin';
-import { AuthLoginStoreState } from '../auth/login/auth-login.store';
-import { authStoreModule } from '../auth/auth.store';
 import {
   appNotificationStoreModule,
   AppNotificationStoreState,
 } from './notification/app-notification.store';
-import { userStoreModule, UserStoreState } from '../user/user.store';
+import { localStorageStorePlugin } from './app.store.plugin';
 
 export interface RootState {
   appName: string;
   post: PostStoreState;
   layout: LayoutStoreState;
-  auth: AuthLoginStoreState;
+  auth: AuthStoreState;
   notification: AppNotificationStoreState;
   user: UserStoreState;
 }
