512fd746cf3f27fd334650333a4a64726437d408
SHctt <SHcttMBG@protonmail.com>
2023-01-13 07:27:23 +0800

    创建文章列表单元媒体

--- src/post/index/components/post-list-item.vue
+++ src/post/index/components/post-list-item.vue
@@ -1,16 +1,15 @@
 <template>
-  <h3>
-    <router-link :to="{ name: 'postShow', params: { postId: item.id } }">
-      {{ item.title }}
-    </router-link>
-    - <small>{{ item.user.name }}</small>
-  </h3>
+  <div class="post-list-item">
+    <post-list-item-media :item="item" />
+  </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import postListItemMedia from './post-list-item-media.vue';
 
 export default defineComponent({
+  components: { postListItemMedia },
   props: {
     item: Object,
   },
