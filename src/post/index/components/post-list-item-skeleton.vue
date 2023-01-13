d49ccb7c2748ec916e78675a35d2b32fdf010686
SHctt <SHcttMBG@protonmail.com>
2023-01-13 18:40:26 +0800

    创建文章列表骨架组件

---
+++ src/post/index/components/post-list-item-skeleton.vue
@@ -0,0 +1,75 @@
+<template>
+  <div :class="postListItemClasses">
+    <div class="post-list-item">
+      <div class="post-list-item-media"></div>
+      <div :class="showPostListItemContent">
+        <div class="thumbnail"></div>
+        <div class="deader">
+          <div class="text"></div>
+          <div class="meta"></div>
+        </div>
+        <div class="actions">
+          <div class="action"></div>
+          <div class="action"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PostListItemSkeleton',
+
+  /**
+   * 属性
+   */
+  props: {},
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {
+    ...mapGetters({
+      layout: 'post/index/layout',
+    }),
+
+    postListItemClasses() {
+      return ['post-list-item', 'skeleton', this.layout];
+    },
+
+    showPostListItemContent() {
+      return !this.layout.includes('-minimal');
+    },
+  },
+
+  /**
+   * 已创建
+   */
+  created() {
+    //
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {},
+
+  /**
+   * 使用组件
+   */
+  components: {},
+});
+</script>
+
+<style scoped></style>
