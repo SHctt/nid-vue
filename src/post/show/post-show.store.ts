a5fd1d9daf93c74541a03fa28a35986cf56a7a1e
SHctt <SHcttMBG@protonmail.com>
2023-01-16 17:54:52 +0800

    修正：内容导航路由地址错误

--- src/post/show/post-show.store.ts
+++ src/post/show/post-show.store.ts
@@ -130,14 +130,13 @@ export const postShowStoreModule: Module<PostShowStoreState, RootState> = {
     async goGetPrevPost({ getters, dispatch }) {
       if (!getters.canNavigateBack) return;
       try {
-        const response = await dispatch('getPostById', getters.prevPost.id);
+        const prevPostId = getters.prevPost.id;
+        const response = await dispatch('getPostById', prevPostId);
 
-        if (getters.prevPost) {
-          appRouter.replace({
-            name: 'postShow',
-            params: { postId: getters.prevPost.id },
-          });
-        }
+        appRouter.replace({
+          name: 'postShow',
+          params: { postId: prevPostId },
+        });
 
         return response;
       } catch (error) {
@@ -153,14 +152,13 @@ export const postShowStoreModule: Module<PostShowStoreState, RootState> = {
       }
 
       try {
-        const response = await dispatch('getPostById', getters.nextPost.id);
+        const nextPostId = getters.nextPost.id;
+        const response = await dispatch('getPostById', nextPostId);
 
-        if (getters.nextPost) {
-          appRouter.replace({
-            name: 'postShow',
-            params: { postId: getters.nextPost.id },
-          });
-        }
+        appRouter.replace({
+          name: 'postShow',
+          params: { postId: nextPostId },
+        });
 
         return response;
       } catch (error) {
