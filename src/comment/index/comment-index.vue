740b883e42e09fe9b6f84f832ad92f8f89a626ff
SHctt <SHcttMBG@protonmail.com>
2023-01-17 00:42:30 +0800

    创建评论索引页面组件

--- src/comment/index/comment-index.vue
+++ src/comment/index/comment-index.vue
@@ -1,15 +1,12 @@
 <template>
-  <div class="comment-side-sheet">
-    <comment-index />
-  </div>
+  <div class="comment-index">comment-index</div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-import CommentIndex from '../index/comment-index.vue';
 
 export default defineComponent({
-  name: 'CommentSideSheet',
+  name: 'CommentIndex',
 
   /**
    * 属性
@@ -43,10 +40,10 @@ export default defineComponent({
   /**
    * 使用组件
    */
-  components: { CommentIndex },
+  components: {},
 });
 </script>
 
 <style scoped>
-@import './styles/comment-side-sheet.css';
+@import './styles/comment-index.css';
 </style>
